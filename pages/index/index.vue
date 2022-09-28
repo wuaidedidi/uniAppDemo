5c1d6f7c60f92e53451600035b02999adb0ff0d2
wuaidedidi <470357714@qq.com>
2022-09-28 21:40:47 +0800

    [update] 页面暂时的更新

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,42 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
+	<view class="container">
+		<view class="top-panel">
+			<view class="title-spacing"></view>
+			<!-- 背景色区域s -->
+			<view class="top-background"></view>
+			<!-- circular 循环 -->
+			<swiper class="swiper" circular indicator-dots autoplay interval="interval" duration="500">
+				<swiper-item><view class="swiper-item uni-bg-red">A</view></swiper-item>
+				<swiper-item><view class="swiper-item uni-bg-green">B</view></swiper-item>
+				<swiper-item><view class="swiper-item uni-bg-blue">C</view></swiper-item>
+			</swiper>
+			s
+		</view>
 		<view class="text-area">
-			<text class="title">{{title}}</text>
+			<text class="title">{{ title }}</text>
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+	data() {
+		return {
+			title: 'Hello'
+		};
+	},
+	onLoad() {},
+	methods: {}
+};
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+<style lang="scss" scoped>
+.top-panel {
+	.top-background {
+		height: 426rpx;
+		background-color: rgb(203, 87, 60);
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+	.title-spacing {
 	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+}
 </style>
